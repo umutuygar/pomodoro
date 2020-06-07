f5ef34f2b6007f0fdef7600a96578590af8339ea
umutuygar <umutuygar@gmail.com>
2020-06-07 14:01:46 +0430

    Added sounds and session logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   minutes: number = 0;
-  seconds: number = 15;
-  counter = { min: this.minutes, sec: this.seconds };
+  seconds: number = 5;
   intervalId;
   isPaused = true;
   isReset = true;
-  startButtonText = "Start";
+  
+  sessionCounters = [
+    { min: this.minutes, sec: this.seconds },
+    { min: 0, sec: 3 },
+    { min: this.minutes, sec: this.seconds },
+    { min: 0, sec: 3 },
+    { min: this.minutes, sec: this.seconds },
+    { min: 0, sec: 3 },
+    { min: this.minutes, sec: this.seconds },
+  ];
+  session: number = 0;
+  counter = this.sessionCounters[this.session];
 
+  playStart() {
+    let audio = new Audio();
+    audio.src = "../assets/audio/start.wav";
+    audio.load();
+    audio.play();
+  }
+
+  playEnd() {
+    let audio = new Audio();
+    audio.src = "../assets/audio/buzzer.wav";
+    audio.load();
+    audio.play();
+  }
+  
   startTimer() {
+    if (this.counter == this.sessionCounters[0]) this.playStart();
     this.intervalId = setInterval(() => {
       if (this.counter.sec - 1 == -1) {
         this.counter.min -= 1;
@@ -23,8 +48,13 @@ export class AppComponent {
       else this.counter.sec -= 1;
       console.log(`${this.counter.min}:${this.counter.sec}`);
       if (this.counter.min === 0 && this.counter.sec == 0) {
-        clearInterval(this.intervalId);
-        this.counter = { min: this.minutes, sec: this.seconds };
+        if (this.session==6) {
+          this.resetClicked();
+        } else {
+          this.playEnd();
+          this.session += 1;
+          this.counter = this.sessionCounters[this.session];
+        }
       }
     }, 1000)
   }
@@ -33,17 +63,25 @@ export class AppComponent {
     this.isPaused = !this.isPaused;
     if (this.isPaused) {
       clearInterval(this.intervalId);
-      this.startButtonText = "Start";
     } else {
       this.startTimer();
-      this.startButtonText = "Pause";
       this.isReset = false;
     }
   }
 
   resetClicked() {
     clearInterval(this.intervalId);
-    this.counter = { min: this.minutes, sec: this.seconds };
+    this.session = 0;
+    this.sessionCounters = [
+      { min: this.minutes, sec: this.seconds },
+      { min: 0, sec: 3 },
+      { min: this.minutes, sec: this.seconds },
+      { min: 0, sec: 3 },
+      { min: this.minutes, sec: this.seconds },
+      { min: 0, sec: 3 },
+      { min: this.minutes, sec: this.seconds },
+    ];
+    this.counter = this.sessionCounters[this.session];
     this.isPaused = true;
     this.isReset = true;
   }
