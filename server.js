73ab550fa6cced8aad2ce69cc8f86302e171d2db
umutuygar <umutuygar@gmail.com>
2020-06-07 14:44:57 +0430

    Added server.js for deployment

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./dist/pomodoro'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/pomodoro/'});
+});
+
+app.listen(process.env.PORT || 8080);
